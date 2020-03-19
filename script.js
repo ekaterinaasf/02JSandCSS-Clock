398a5e60c9cda1dfb9a30cf34bc20bb99191ed00
Ekaterina <nataveres@gmail.com>
2020-03-19 11:41:11 +0100

    Script in html

--- script.js
+++ script.js
@@ -7,11 +7,15 @@ function setDate() {
 
   const seconds = now.getSeconds();
   const secondsDegrees = (seconds / 60) * 360 + 90;
+  //console.log(`"rotate(${secondsDegrees}deg)"`);
+  //console.log(typeof `rotate(${secondsDegrees}deg)`);
+  //console.log(typeof secondHand.style.transform);
   secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
 
   const mins = now.getMinutes();
   const minsDegrees = (mins / 60) * 360 + (seconds / 60) * 6 + 90;
-  minsHand.style.transform = `rotate(${minsDegrees}deg)`;
+  minsHand.style.transform = "rotate(" + minsDegrees + "deg)";
+  //minsHand.style.transform = "rotate(100deg)";
 
   const hour = now.getHours();
   const hourDegrees = (hour / 12) * 360 + (mins / 60) * 30 + 90;
